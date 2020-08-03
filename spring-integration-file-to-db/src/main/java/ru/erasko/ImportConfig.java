757f8a73933648339e95a26488a28f3360a7c74c
Elmira Skorobogatova <elmirasco@gmail.com>
2020-08-03 14:10:19 +0200

    Добавила код для работы с БД

--- spring-integration-file-to-db/src/main/java/ru/erasko/ImportConfig.java
+++ spring-integration-file-to-db/src/main/java/ru/erasko/ImportConfig.java
@@ -2,23 +2,39 @@ package ru.erasko;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.ConsumerEndpointSpec;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.transformer.FileToStringTransformer;
+import org.springframework.integration.jpa.dsl.Jpa;
+import org.springframework.integration.jpa.dsl.JpaUpdatingOutboundEndpointSpec;
+import org.springframework.integration.jpa.support.PersistMode;
+import ru.erasko.model.Brand;
 
-
+import javax.persistence.EntityManagerFactory;
 import java.io.File;
 
 @Configuration
 public class ImportConfig {
     private static final Logger logger = LoggerFactory.getLogger(ImportConfig.class);
 
+
     @Value("${source-dir-path}")
     private String sourceDirectoryPath;
-    @Value("${dest-dir-path}")
-    private String destDirectoryPath;
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    @Autowired
+    public ImportConfig(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
 
     @Bean
     public MessageSource<File> sourceDirectory() {
@@ -28,17 +44,27 @@ public class ImportConfig {
         return messageSource;
     }
 
-//    @Bean
-//    public JpaExecutor jpaExecutor() {
-//        JpaExecutor jpaExecutor = new JpaExecutor();
-//        return jpaExecutor;
-//    }
+    @Bean
+    public JpaUpdatingOutboundEndpointSpec jpaPersistHandler() {
+        return Jpa.outboundAdapter(this.entityManagerFactory)
+                .entityClass(Brand.class)
+                .persistMode(PersistMode.PERSIST);
+    }
 
-//    @Bean
-//    public IntegrationFlow jpaFlow(JpaExecutor jpaExecutor) {
-//        return IntegrationFlows.from(new JpaPollingChannelAdapter(jpaExecutor))
-//                .split()
-//                .transform()
-//    }
+    @Bean
+    public IntegrationFlow fileMoveFlow() {
+        return IntegrationFlows.from(sourceDirectory(), conf -> conf.poller(Pollers.fixedDelay(2000)))
+                .filter(msg -> ((File) msg).getName().endsWith(".csv"))
+                .transform(new FileToStringTransformer())
+                .split(s -> s.delimiters("\n"))
+                .<String, Brand>transform(name -> {
+                    Brand brand = new Brand();
+                    brand.setName(name);
+                    return brand;
+                })
+                //.handle(destDirectory())
+                .handle(jpaPersistHandler(), ConsumerEndpointSpec::transactional)
+                .get();
+    }
 
 }
